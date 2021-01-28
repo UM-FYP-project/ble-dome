bc9778566fb030b959a5996b2f9d4b004a77faa9
UM <iohongwal@gmail.com>
2021-01-28 10:56:24 +0800

    28/01 10:55

--- ble dome/ble_domeApp.swift
+++ ble dome/ble_domeApp.swift
@@ -18,15 +18,20 @@ struct ble_domeApp: App {
     
 }
 
+struct Peripheral: Identifiable {
+    let id : Int
+    let name : String
+    let rssi : Int
+}
+
 class BLE: NSObject, ObservableObject, CBCentralManagerDelegate{
     var centralManager:CBCentralManager!
-    @Published var  isBluetoothON : Bool = false
-    //initialize BLE
-//    func initi_BLE(){
-//        print("initiBLE")
-//        centralManager  = CBCentralManager.init(delegate: self, queue: nil)
-//        centralManager.delegate = self
-//    }
+    var connectedPeripheral: CBPeripheral?
+    var connectedPeripheral_record: String?
+    @Published var isBluetoothON : Bool = false
+    @Published var peripherals = [Peripheral]()
+    @Published var peripheral_count : Int = 0
+
     override init() {
         super.init()
         centralManager = CBCentralManager(delegate: self, queue: nil)
@@ -54,6 +59,19 @@ class BLE: NSObject, ObservableObject, CBCentralManagerDelegate{
     func stopscan_device() {
         self.centralManager.stopScan()
         print("Scan Stopped")
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        var peripheralName : String!
+        if let name = advertisementData[CBAdvertisementDataLocalNameKey] as? String {
+           peripheralName = name
+        }
+        else{
+            peripheralName = "Unkown"
+        }
+        let newPeripheral = Peripheral(id: peripherals.count, name: peripheralName, rssi: RSSI.intValue)
+        print(newPeripheral)
+        peripherals.append(newPeripheral)
     }
     
 }
